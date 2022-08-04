b26e6d4fc2a0ff17fc54a15667d610d75327a650
Lucas <lucasmoraes7551@gmail.com>
2022-08-03 23:53:31 -0300

    Register models in admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,45 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, ExpenseGroup, Regarding, Wallet, PaymentMethod, Payment, Expense, Tag, Item
 
 admin.site.register(User, UserAdmin)
+
+
+@admin.register(ExpenseGroup)
+class ExpenseGroupAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Regarding)
+class RegardingGroupAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PaymentMethod)
+class PaymentMethodAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
