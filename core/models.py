fde850bf28d22f512b78ccad11b95b10df450d64
Lucas <lucasmoraes7551@gmail.com>
2022-08-06 11:54:32 -0300

    Adjust some model fields and define __str__

--- core/models.py
+++ core/models.py
@@ -21,6 +21,9 @@ class ExpenseGroup(BaseModel):
     members = models.ManyToManyField("User", related_name="expenses_groups")
     is_active = models.BooleanField("Is active?", default=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Regarding(BaseModel):
     name = models.CharField("Name", max_length=128)
@@ -31,9 +34,15 @@ class Regarding(BaseModel):
     is_closed = models.BooleanField("Is closed?", default=False)
     balance_json = models.JSONField("Balance Data", default=dict, null=True)
 
+    def __str__(self):
+        return f"{self.expense_group.name} - {self.name} - ({self.description})"
+
 
 class Wallet(BaseModel):
     owner = models.OneToOneField("User", related_name="wallet", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.owner}"
 
 
 class PaymentMethod(BaseModel):
@@ -45,8 +54,11 @@ class PaymentMethod(BaseModel):
     description = models.TextField("Description", null=True, blank=True)
     wallet = models.ForeignKey("Wallet", related_name="payment_methods", on_delete=models.PROTECT)
     limit = models.DecimalField("Payment Limit Value", null=True, max_digits=14, decimal_places=4)
-    compensation_date = models.DateField("Payment Due Date", default=datetime.today().date())
+    compensation_day = models.IntegerField("Payment Compensation Day", null=True, blank=True)
     is_active = models.BooleanField("Payment Method is active?", default=True)
+
+    def __str__(self):
+        return f"{self.wallet} {self.type} - {self.description}"
 
 
 class Payment(BaseModel):
@@ -61,6 +73,9 @@ class Payment(BaseModel):
     expense = models.ForeignKey("Expense", related_name="payments", on_delete=models.PROTECT)
     payment_status = models.CharField("Payment Status", default=PaymentStatuses.AWAITING_VALIDATION, max_length=128, choices=PaymentStatuses.choices)
 
+    def __str__(self):
+        return f"{self.expense} - {self.payer}"
+
 
 class Expense(BaseModel):
     name = models.CharField("Name", max_length=128)
@@ -71,18 +86,27 @@ class Expense(BaseModel):
     validated_by = models.ManyToManyField("User", related_name="validated_expenses")
     is_validated = models.BooleanField("Is validated?", default=False)
 
+    def __str__(self):
+        return f"{self.regarding.expense_group} - {self.regarding.name} - {self.name} - R${self.cost:.2f}"
+
 
 class Tag(BaseModel):
+    name = models.CharField("Tag", max_length=128, default="Outros")
     owner = models.ForeignKey("User", related_name="created_tags", on_delete=models.PROTECT)
     expenses_groups = models.ManyToManyField("ExpenseGroup", related_name="items_tags")
+
+    def __str__(self):
+        return self.name
 
 
 class Item(BaseModel):
     name = models.CharField("Name", max_length=128)
-    tags = models.ForeignKey("Tag", related_name="items", on_delete=models.PROTECT)
+    tags = models.ForeignKey("Tag", related_name="items", on_delete=models.PROTECT, null=True, blank=True)
     price = models.DecimalField("Price", max_digits=14, decimal_places=4)
     expense = models.ForeignKey("Expense", related_name="items", on_delete=models.PROTECT)
     consumers = models.ManyToManyField("User", related_name="items_purchased")
 
+    def __str__(self):
+        return f"{self.expense.regarding} - {self.expense.name} - {self.name} - R${self.price:.2f}"
 
 
