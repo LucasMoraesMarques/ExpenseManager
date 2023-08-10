4c9bc5314edb65f7c92992b64f7f5eb62fb6fe28
Lucas <lucasmoraes7551@gmail.com>
2023-08-10 17:47:02 -0300

    Collect static files and config settings

--- ExpenseManager/settings.py
+++ ExpenseManager/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,6 +58,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -130,7 +133,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -163,3 +172,4 @@ if DEBUG:
         '0.0.0.0',
         '192.168.0.104'
     ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
--- ExpenseManager/urls.py
+++ ExpenseManager/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 import debug_toolbar
 
 urlpatterns = [path("admin/", admin.site.urls),
                path("api/v1/", include("core.urls"))]
 
-
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
 if settings.DEBUG:
     urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
